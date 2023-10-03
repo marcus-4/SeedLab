e0eaa4fdb13ce2d40be910aade06940b5004f116
marcus-4 <marcus@jkbecker.com>
2023-10-02 23:26:40 -0600

    Update readme.md

--- MiniProjectMain/readme.m
+++ MiniProjectMain/readme.m
@@ -1,1 +1,1 @@
-THis is the arduino code section of the mini project, which receives serial data and implements the motor control loop
+THis is the final code for the mini project, which receives serial data and implements the motor control loop
